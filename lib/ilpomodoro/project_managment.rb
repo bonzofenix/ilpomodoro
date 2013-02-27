cb46767a124fbf8e0d2975169f72061a1f2f7776
bonzofenix <bonzofenix@gmail.com>
2013-02-26 21:51:49 -0300

    pivotal integration almost finished

---
+++ lib/ilpomodoro/project_managment.rb
@@ -0,0 +1,69 @@
+require 'pivotal-tracker'
+PivotalTracker::Project.class_eval do
+  def to_s
+    self.name
+  end
+end
+
+PivotalTracker::Story.class_eval do
+  def to_s
+    "(#{self.current_state}) #{self.name}"    
+  end
+end
+
+class Ilpomodoro::ProjectManagement
+  @@service = nil
+  @@project = nil
+
+  class << self
+    def login
+      PivotalTracker::Client.token(get_username, get_password)   
+    end
+
+    def choose_from_projects
+      @@project = choose do |m|
+        m.header = 'in which project you will be working on?'
+        projects.each do |p|
+          m.choice p 
+        end
+      end
+    end
+
+    def choose_from_services
+      @@service = choose do |m|
+        m.header = 'do you use any of this services to manage tasks?'
+        services.each do |p|
+          m.choice p
+        end
+      end
+    end
+
+    def tasks
+      project.stories.all(current_state: ['unscheduled','started'])
+    end
+
+    def services 
+      ['pivotaltracker']
+    end
+
+    def projects
+      PivotalTracker::Project.all
+    end
+
+    def get_username
+      ask("enter your #{@service} username:   ")
+    end
+    
+    def get_password
+      ask("enter your #{@service} password:   "){ |q| q.echo = 'x' } 
+    end
+    
+    def project
+      @@project
+    end
+
+    def service
+      @@service
+    end
+  end
+end
---
+++ spec/ilpomodoro/project_management_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe Ilpomodoro::ProjectManagement do
+  let(:project_management){ Ilpomodoro::ProjectManagement }
+  let(:pivotal_task){ PivotalTracker::Story.new( name: 'a task') }
+  let(:pivotal_tasks){ [pivotal_task] }
+  let(:pivotal_project){ PivotalTracker::Project.new( name: 'a project') }
+  let(:pivotal_projects){ [pivotal_project] }
+
+  it '.login' do
+    project_management.should_receive(:ask).and_return('a username', 'a password')
+    PivotalTracker::Client.should_receive(:token).with('a username', 'a password')
+    project_management.login
+  end
+
+  it '.services' do
+    project_management.services.should == ['pivotaltracker']
+  end
+
+  it '.tasks' do
+    Ilpomodoro::ProjectManagement.stub(project: stub(stories: stub(all: pivotal_tasks)))
+    project_management.tasks.should == pivotal_tasks
+  end
+  it '.projects' do
+    PivotalTracker::Project.should_receive(:all).and_return(pivotal_projects)
+    project_management.projects.should == pivotal_projects
+  end
+
+  describe '.choose_from_services' do
+    it 'display the options' do
+    project_management.should_receive :choose
+    project_management.choose_from_services
+    end
+
+    it 'saves into a class variable the choosen project' do
+      project_management.should_receive(:choose).and_return('the project')
+      expect do
+        project_management.choose_from_projects
+      end.to change{Ilpomodoro::ProjectManagement.project }.from(nil).to('the project')
+    end
+
+    it 'saves into a class variable the choosen service' do
+      project_management.should_receive(:choose).and_return('pivotaltracker')
+      expect do
+        project_management.choose_from_services
+      end.to change{project_management.service }.from(nil).to('pivotaltracker')
+    end
+  end
+
+  it '.choose_from_projects' do
+    project_management.should_receive :choose
+    project_management.choose_from_projects
+  end
+end
